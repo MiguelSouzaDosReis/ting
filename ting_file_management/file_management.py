6917641afb0aec9221d433b3255c29055f9bc189
Miguel_Reis <guelsouzar@gmail.com>
2022-10-27 04:16:04 -0300

    req 02 done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+from os.path import exists
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if exists(path_file) is False:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+    with open(path_file, mode="r") as file:
+        data = list(file)
+        listData = []
+        for row in data:
+            newLine = row.rstrip("\n")
+            listData.append(newLine)
+    return listData
