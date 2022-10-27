6691fb04b65ae0ced71b07e3daaff08e85f4a761
Miguel_Reis <guelsouzar@gmail.com>
2022-10-27 04:18:48 -0300

    fix the req 02 and done the req 03 and 04 and 05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,38 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        dic = instance.search(index)
+        if dic["nome_do_arquivo"] == path_file:
+            return None
+    lines = txt_importer(path_file)
+    obj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+    instance.enqueue(obj)
+    print(obj)
+    return
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos")
+        return
+    else:
+        remove = instance.dequeue()
+        path_remove = remove["nome_do_arquivo"]
+        print(f"Arquivo {path_remove} removido com sucesso")
+        return
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        find = instance.search(position)
+        print(find)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
+        return
