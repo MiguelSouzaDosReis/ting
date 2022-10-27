18f5c1cf0a21b187479db932f62c5dcf2f856b4c
Miguel_Reis <guelsouzar@gmail.com>
2022-10-27 07:27:09 -0300

    req 06 and 07 done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,38 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    array = []
+    for rows in range(len(instance)):
+        ocorrencias = []
+        lines = instance.search(rows)["linhas_do_arquivo"]
+        for index, line in enumerate(lines):
+            if word.lower() in line.lower():
+                path_file = instance.search(rows)["nome_do_arquivo"]
+                count = index + 1
+                ocorrencias.append({"linha": count})
+        if len(ocorrencias) > 0:
+            obj = {
+                "palavra": word,
+                "arquivo": path_file,
+                "ocorrencias": ocorrencias
+                }
+            array.append(obj)
+    return array
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    array = []
+    for rows in range(len(instance)):
+        ocorrencias = []
+        lines = instance.search(rows)["linhas_do_arquivo"]
+        for index, line in enumerate(lines):
+            if word.lower() in line.lower():
+                path_file = instance.search(rows)["nome_do_arquivo"]
+                count = index + 1
+                ocorrencias.append({"linha": count, "conteudo": line})
+        if len(ocorrencias) > 0:
+            obj = {
+                "palavra": word,
+                "arquivo": path_file,
+                "ocorrencias": ocorrencias
+                }
+            array.append(obj)
+    return array
